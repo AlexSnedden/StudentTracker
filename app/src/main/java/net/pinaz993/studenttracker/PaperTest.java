462859af27a303b0427e43577273426bb5b8abca
AlexSnedden <alexander.jay.snedden@gmail.com>
2017-09-13 11:19:03 -0400

    Continued paper testing.

--- app/src/main/java/net/pinaz993/studenttracker/PaperTest.java
+++ app/src/main/java/net/pinaz993/studenttracker/PaperTest.java
@@ -1,9 +1,46 @@
 package net.pinaz993.studenttracker;
 
+import io.paperdb.Paper;
 
 /**
- * Created by asnedden on 9/13/2017.
+ * Created by Alexander Snedden on 9/13/2017.
  */
 
 public class PaperTest {
+    private final int integer;
+    private final double decimal;
+    private final String word;
+    private final String ID;
+
+    PaperTest(int integer, double decimal, String word, String ID) {
+        this.integer = integer;
+        this.decimal = decimal;
+        this.word = word;
+        this.ID = ID;
+    }
+
+    PaperTest(String ID) {
+        this.ID = ID;
+        this.integer = Paper.book(ID).read("integer");
+        this.decimal = Paper.book(ID).read("decimal");
+        this.word = Paper.book(ID).read("word");
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public double getDecimal() {
+        return decimal;
+    }
+
+    public int getInteger() {
+        return integer;
+    }
+
+    public void saveObject() {
+        Paper.book(ID).write("integer", integer);
+        Paper.book(ID).write("decimal", decimal);
+        Paper.book(ID).write("word", word);
+    }
 }
