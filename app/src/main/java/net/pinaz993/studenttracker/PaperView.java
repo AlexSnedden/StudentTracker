c8cf609b0342d48427f7b128dd0ef413438f02e3
AlexSnedden <alexander.jay.snedden@gmail.com>
2017-09-13 13:19:58 -0400

    Paper test finished

--- app/src/main/java/net/pinaz993/studenttracker/PaperView.java
+++ app/src/main/java/net/pinaz993/studenttracker/PaperView.java
@@ -1,5 +1,6 @@
 package net.pinaz993.studenttracker;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,13 +21,16 @@ public class PaperView extends AppCompatActivity {
         final TextView integerView = (TextView)findViewById(R.id.integerView);
         final TextView decimalView = (TextView)findViewById(R.id.decimalView);
 
+        final Snackbar failure = Snackbar.make(viewBtn, "object not found", Snackbar.LENGTH_SHORT);
+
         viewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PaperTest object = PaperTest.getObject(nameInput.getText().toString());
                 wordView.setText(object.getWord());
-                integerView.setText(object.getInteger());
+                integerView.setText(object.getInteger().toString());
                 decimalView.setText(object.getDecimal().toString());
+
             }
         });
     }
