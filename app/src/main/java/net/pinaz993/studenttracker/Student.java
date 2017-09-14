10687558f1e0e66e6746dcb0c3ebf8a57c98389e
AlexSnedden <alexander.jay.snedden@gmail.com>
2017-09-14 16:28:16 -0400

    Implementing Student record handling.

--- app/src/main/java/net/pinaz993/studenttracker/Student.java
+++ app/src/main/java/net/pinaz993/studenttracker/Student.java
@@ -1,6 +1,8 @@
 package net.pinaz993.studenttracker;
 
-import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * Created by Patrick Shannon on 8/30/2017.
@@ -13,7 +15,8 @@ public class Student {
     private final String email;
     private final String studentID;
     private boolean delinquent; // condition for read formatting on the student's name
-
+    private ArrayList<AttendanceRecord> attendanceRecords;
+    private transient AttendanceSummary summary;
     /**
      *
      * @param firstName First name of student
@@ -28,7 +31,8 @@ public class Student {
         this.lastName = lastName;
         this.email = email;
         this.studentID = studentID;
-        //TODO: Finish constructor for Student
+        this.attendanceRecords = new ArrayList<>();
+        this.summary = null;
     }
 
     /**
@@ -43,7 +47,8 @@ public class Student {
         this.lastName = lastName;
         this.studentID = studentID;
         this.email = null;
-        //TODO: Finish email-less constructor for Student
+        this.attendanceRecords = new ArrayList<>();
+        this.summary = null;
     }
 
     /**
@@ -51,31 +56,23 @@ public class Student {
      * @param present If the student was there at all
      * @param lateArrival If the student arrived late to class
      * @param earlyDeparture If the student left class early
-     * @param time Timestamp object for recording date and time of record
      */
-    public void recordAttendace(boolean present, boolean lateArrival,
-                                boolean earlyDeparture, Timestamp time){
-        //If not present, just mark that.
-
-        //else if present, but late or early leave, record that
-
-        //else, record present
-
-        //Use db to save record with timestamp
-
-        //TODO: Finish Student.recordAttendance()
+    public void recordAttendance(boolean present, boolean lateArrival, boolean earlyDeparture){
+        this.attendanceRecords.add(new AttendanceRecord(present, lateArrival, earlyDeparture));
     }
 
-    public void compileAttendanceReport(){
-        //Should compile total number of days absent, days late, days left early
-        //For use in visual report compilation
+    public void recordDatedAttendance(boolean present, boolean lateArrival, boolean earlyDeparture,
+                                      Date timestamp) {
+        AttendanceRecord record = new AttendanceRecord(timestamp, present, lateArrival,
+                                                       earlyDeparture);
+        this.attendanceRecords.add(record);
+    }
 
-        //Go to db and fetch all records pertaining to the current student
-
-        //Add up all days absent
-
-        //Add up all days present and
-        //TODO: Finish Student.compileAttendanceReport()
+    public void compileAttendanceSummary(){
+        /*
+         * This is a method because we don't wish to perform each time student object is created.
+         */
+        this.summary = new AttendanceSummary(this.attendanceRecords);
     }
 
 
@@ -86,6 +83,46 @@ public class Student {
     public void setDelinquent(boolean delinquent) {
         this.delinquent = delinquent;
     }
+
+    public void save() {
+        // TODO: finish Student.save()
+    }
 }
 
+class AttendanceRecord {
+    Date timestamp;
+    boolean present, lateArrival, earlyDeparture;
 
+    AttendanceRecord(boolean present, boolean lateArrival, boolean earlyDeparture) {
+        this.present = present;
+        this.lateArrival = lateArrival;
+        this.earlyDeparture = earlyDeparture;
+        this.timestamp = new Date();
+    }
+
+    AttendanceRecord(Date timestamp, boolean present, boolean lateArrival, boolean earlyDeparture) {
+        this.timestamp = timestamp;
+        this.present = present;
+        this.lateArrival = lateArrival;
+        this.earlyDeparture = earlyDeparture;
+    }
+}
+
+class AttendanceSummary {
+    int daysAbsent;
+    int daysPresent;
+    int lateArrivals;
+    int earlyDepartures;
+
+    AttendanceSummary(ArrayList<AttendanceRecord> attendanceRecords){
+        Iterator<AttendanceRecord> I = attendanceRecords.listIterator();
+        AttendanceRecord currentRecord;
+        while(I.hasNext()) {
+            currentRecord = I.next();
+            if(currentRecord.present) daysPresent++;
+            else daysAbsent++;
+            if(currentRecord.lateArrival) lateArrivals++;
+            if(currentRecord.earlyDeparture) earlyDepartures++;
+        }
+    }
+}
