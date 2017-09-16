38165841d00918a6dff91b7bc6ab5758f25b04fa
AlexSnedden <alexander.jay.snedden@gmail.com>
2017-09-15 21:45:06 -0400

    Continued development on ClassList.

--- app/src/main/java/net/pinaz993/studenttracker/ClassList.java
+++ app/src/main/java/net/pinaz993/studenttracker/ClassList.java
@@ -1,5 +1,9 @@
 package net.pinaz993.studenttracker;
 
+import java.util.ArrayList;
+
+import io.paperdb.Paper;
+
 /**
  * Created by Patrick Shannon on 8/30/2017.
  */
@@ -8,18 +12,22 @@ public class ClassList {
     /**
      * A list of students in a single class.
      */
+    final static String BOOK_ID = "ClassLists";
+    private String classListID;
+    private ArrayList<Student> students;
 
     public static ClassList retrieve(String classListID){
         /*
         grab the classList from Paper with this ID
          */
-        return null;
+        return Paper.book(BOOK_ID).read(classListID);
     }
 
     public void save(){
         /*
         save this object to disk with array list of student ids
          */
+        Paper.book(BOOK_ID).write(this.classListID, this);
     }
 
     public void putToBed(){
@@ -31,6 +39,8 @@ public class ClassList {
         suicide
 
          */
+        //TODO: implement ClassList.putToBed
+
     }
 
 }
