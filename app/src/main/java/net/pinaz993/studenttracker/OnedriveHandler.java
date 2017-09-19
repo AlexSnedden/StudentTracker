99125360276ccf50aacc278967d50db53be9099c
AlexSnedden <alexander.jay.snedden@gmail.com>
2017-09-19 16:31:50 -0400

    Started implemeting OnedriveHandler class.

---
+++ app/src/main/java/net/pinaz993/studenttracker/OnedriveHandler.java
@@ -0,0 +1,13 @@
+package net.pinaz993.studenttracker;
+
+import com.mashape.unirest.http.Unirest;
+
+/**
+ * Created by Alexander Snedden on 9/19/17.
+ */
+
+public class OnedriveHandler {
+
+    private final String userAuthenticationUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
+    
+}
