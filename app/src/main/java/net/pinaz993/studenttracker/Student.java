f95e17d7ee0e9fc2d08adbef7cd85387524bb2fb
Patrick Shannon <pinaz993@gmail.com>
2017-09-26 14:47:50 -0400

    Cleaned up Student class in preparation for SQL implementation of attendance records

--- app/src/main/java/net/pinaz993/studenttracker/Student.java
+++ app/src/main/java/net/pinaz993/studenttracker/Student.java
@@ -1,10 +1,27 @@
 package net.pinaz993.studenttracker;
+import android.support.annotation.Nullable;
+
+import java.sql.Timestamp;
+
 import io.paperdb.Paper;
 
 
 /**
  * Created by Patrick Shannon on 8/30/2017.
- * This packacge is for the Student Object, and all related objects.
+ * This package is for the Student Object, and all related objects.
+ * The student object holds information about the student, and is responsible for keeping track of all
+ * records pertaining to the student, such as behavior and attendance.
+ *
+ * The student is stored using the Paper library by pilgr. A static method is provided to retrieve
+ * the object from storage. Each student object is responsible for saving itself to storage when
+ * told.
+ *
+ * When data needs to be recorded about the student, the object records this data in an SQL database
+ * on the device. All student records share the same database. Each record is accompanied by a
+ * Student ID string, a Class ID string, and a timestamp.
+ *
+ * If a record needs to be edited, the object will query for the record and update it. It will also
+ * be able to delete records.
  */
 
 public class Student {
@@ -20,7 +37,7 @@ public class Student {
     }
 
     /**
-     *
+     * Constructor with email address
      * @param firstName First name of student
      * @param lastName Last name of student
      * @param email Email of the student, can be null
@@ -35,7 +52,7 @@ public class Student {
     }
 
     /**
-     *
+     * Constructor with no email address
      * @param firstName First name of student
      * @param lastName Last name of Student
      * @param studentID A value unique to each student. Will be used as a primary key
@@ -47,25 +64,80 @@ public class Student {
         this.studentID = studentID;
         this.email = null;
     }
+
     /**
-     * Takes attendance data and records it in storage
-    public void recordAttendance(){
-            //TODO: Replace with SQL implementation, because querying
+     * Record attendance with the following values:
+     * @param classID The class the student was to attend
+     * @param timestamp The time and date of attendance. Can be null, will be set to now()
+     * @param present Was the student present?
+     * @param lateArrival Did the student arrive late?
+     * @param earlyDeparture Did the student leave class early?
+     * @param excused Was the behavior excused?
+     *
+     * Can also be used to record attendance after the fact.
+     */
+    public void recordAttendence(String classID, @Nullable Timestamp timestamp,
+                                 boolean present, boolean lateArrival,
+                                 boolean earlyDeparture, boolean excused) {
+        //TODO: Implement Student.recordAttendance()
+
+        //Table is already set up, so lets check the data
+        //If the timestamp is null, set it to now()
+
+        //If present is false, late arrival and early departure are automatically false
+
+        //Store it.
+
     }
 
-    public void compileAttendanceSummary(){
-        /*
-         * This is a method because we don't wish to perform each time student object is created.
-         */
+    /**
+     * Change a specified record of attendance. Specified by the following:
+     * @param classID The class the student was to attend
+     * @param timestamp The time and date of attendance.  Cannot be null
+     * Changes the following values in the record:
+     * @param present Was the student present?
+     * @param lateArrival Did the student arrive late?
+     * @param earlyDeparture Did the student leave class early?
+     * @param excused Was the behavior excused?
+     */
+    public void changeAttendance(String classID, Timestamp timestamp,
+                                 boolean present, @Nullable boolean lateArrival,
+                                 @Nullable boolean earlyDeparture, @Nullable boolean excused) {
+        //TODO: Implement Student.changeAttendance()
 
-    public boolean isDelinquent() {return delinquent;}
+        //If present is false, late arrival and early departure are automatically false
 
-    public void setDelinquent(boolean delinquent) {this.delinquent = delinquent;}
+        //If any values are null, make sure they are not changed in the record
+
+        //Change it.
+
+    }
+
+    /**
+     *
+     * @param classID The class the student was to attend
+     * @param present Was the student present?
+     * @param lateArrival Did the student arrive late?
+     * @param earlyDeparture Did the student leave class early?
+     * @param excused Was the behavior excused?
+     */
+    public void changeRecentAttendance(String classID, boolean present, boolean lateArrival,
+                                       boolean earlyDeparture, boolean excused) {
+        //TODO: Implement Student.changeRecentAttendance()
+
+        //Find the record (there should only be one) within the last attendance period, and change
+        //it. The attendance period is defined elsewhere, with a default of one day.
+
+    }
+
 
 
     public void save() {
         Paper.book(BOOK_ID).write(studentID, this);
     }
+
+    //<editor-fold desc="Setters and Getters">
+    public boolean isDelinquent() {return delinquent;}
 
     public String getFirstName() {return firstName;}
 
@@ -76,17 +148,20 @@ public class Student {
     public String getStudentID() {return studentID;}
 
     public String getFullName() {return firstName + " " + lastName;}
+    //</editor-fold>
 
 
-    //TODO: Write a record replacement method in case of inaccuracies
-}
+    /**
+     * Used to summarise all attendance records recorded for a student.
+     * Will not be initialised on creation. Will only be created when needed.
+     * TODO: Integrate with SQL Implementation of attendance records
+     */
+    private class AttendanceSummary {
+        int daysAbsent;
+        int daysPresent;
+        int lateArrivals;
+        int earlyDepartures;
 
-
-class AttendanceSummary {
-    int daysAbsent;
-    int daysPresent;
-    int lateArrivals;
-    int earlyDepartures;
-
-    AttendanceSummary() {}
+        AttendanceSummary() {}
+    }
 }
