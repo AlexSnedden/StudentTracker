ae11bd8e4103a447987281b73b771217ea1b51a7
Patrick Shannon <pinaz993@gmail.com>
2017-10-18 11:20:26 -0400

    Explicitly depended AttendanceInterval on injected settings

--- app/src/main/java/net/pinaz993/studenttracker/AttendanceInterval.java
+++ app/src/main/java/net/pinaz993/studenttracker/AttendanceInterval.java
@@ -9,34 +9,35 @@ public class AttendanceInterval{
     Duration duration;
     Instant start;
     Interval interval;
+    SettingsHandler settings;
 
-    static long getCurrentStart(){
-        return getCurrent().start.getMillis();
+    static long getCurrentStart(SettingsHandler settings){
+        return getCurrent(settings).start.getMillis();
     }
 
-    static AttendanceInterval getCurrent() {
+    static AttendanceInterval getCurrent(SettingsHandler settings) {
         Instant now = new Instant();
-        return getFromInstant(now);
+        return getFromInstant(now,settings);
     }
 
-    static AttendanceInterval getFromInstant(Instant then){
+    static AttendanceInterval getFromInstant(Instant then, SettingsHandler settings){
         LocalDate now = then.toDateTime().toLocalDate();
         Instant newStart;
-        if (SettingsHandler.get().weekly) {
+        if (settings.weekly) {
             now = now.dayOfWeek().withMinimumValue();
             newStart = now.toDateTimeAtStartOfDay().toInstant();
-        } else if(SettingsHandler.get().daily) {
+        } else if(settings.daily) {
             newStart = now.toDateTimeAtStartOfDay().toInstant();
         } else {
             newStart = new Instant(0); //Shouldn't ever happen, but is easy to test for.
         }
-        return new AttendanceInterval(newStart);
+        return new AttendanceInterval(newStart, settings);
     }
 
     // To ensure proper alignment, all initializations will take place in static methods.
-    private AttendanceInterval(Instant start) {
+    private AttendanceInterval(Instant start, SettingsHandler settings) {
         this.start = start;
-        duration = SettingsHandler.get().attendanceIntervalDuration;
+        duration = settings.attendanceIntervalDuration;
         interval = new Interval(start, duration);
     }
 
