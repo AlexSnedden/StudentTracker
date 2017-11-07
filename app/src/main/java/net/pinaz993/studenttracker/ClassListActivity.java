124c02dd103f2ef4b93138861f888e2584cf9240
Patrick Shannon <pinaz993@gmail.com>
2017-11-07 12:40:13 -0500

    Fixed cl_top_bar so that it uses a library that actually works

---
+++ app/src/main/java/net/pinaz993/studenttracker/ClassListActivity.java
@@ -0,0 +1,25 @@
+package net.pinaz993.studenttracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
+
+public class ClassListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_list);
+        final ExpandableLayout optionContainer = (ExpandableLayout)findViewById(R.id.option_container);
+        Button dropMenuBtn = (Button)findViewById(R.id.drop_menu_btn);
+        dropMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                optionContainer.toggle(true);
+            }
+        });
+    }
+}
